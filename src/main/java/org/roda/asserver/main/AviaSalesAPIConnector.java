11db94fcc83bc8287b89923ca1b33fe7379f01cd
kvvn <kvvn@i.kiev.ua>
2015-03-23 18:59:30 +0200

    mysql workers added

--- src/main/java/org/roda/asserver/main/AviaSalesAPIConnector.java
+++ src/main/java/org/roda/asserver/main/AviaSalesAPIConnector.java
@@ -19,11 +19,23 @@ public class AviaSalesAPIConnector {
 
     public String getTickets(String city, String country, String ip, String date){
         try {
+            //where am i
             String location = getLocation(ip);
             db.initConnection("com.mysql.jdbc.Driver", "jdbc:mysql://127.0.0.1:3306/sa_db", "kvvn", "nau08fel");
+            //where i want to go
             String iata = db.getIATA(city, country);
+            System.out.println(date.substring(0, 7));
             System.out.println(location);
+            String url = "http://api.travelpayouts.com/v1/prices/calendar?depart_date=" + date.substring(0, 7) +
+                    "&origin=" + location +
+                    "&destination=" + iata +
+                    "&calendar_type=departure_date&token=fc151c8a1cd5bebf595210752baf1596";
 
+            URL obj = new URL(url);
+            ReqestSender rsender = new ReqestSender();
+            String json = rsender.sendGet(obj);
+
+            System.out.println(json);
         } catch (Exception ex){
             System.out.println(ex);
         }
